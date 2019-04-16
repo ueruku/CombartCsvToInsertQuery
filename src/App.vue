b7d98bf7c45fa35e5729e9d89e6a236f51181b67
ueruku <tenshilove@live.jp>
2019-04-16 17:23:36 +0900

    oracle対応中

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,10 @@
     <div style="text-align: left; font-size: 2em; color: gray;">CSV→InsertQuery</div>
     <div class="input">
       <label>DBの種類</label>
-      <select v-model="DbType">
+      <select v-model="dbType">
         <option>MySQL</option>
         <option>SQL Server</option>
+        <option>Oracle</option>
       </select>
       <label>テキストの種類</label>
       <select v-model="textType">
@@ -49,7 +50,7 @@ export default Vue.extend({
   name: "app",
   data() {
     return {
-      DbType: "MySQL",
+      dbType: "MySQL",
       textType: "TSV",
       tableName: "",
       firstLineName: false,
@@ -61,47 +62,92 @@ export default Vue.extend({
     /** ボックスのスタイル */
     insertQuery(): string {
       let table: string[][] = [];
-      if (this.textType === "TSV") {
-        table = this.combartTsvToTable(this.text);
-      } else if (this.textType === "CSV") {
-        table = this.combartCsvToTable(this.text);
-      }
+      if (this.textType === "TSV") table = this.combartTsvToTable(this.text);
+      if (this.textType === "CSV") table = this.combartCsvToTable(this.text);
 
       // 余計な空行を削除
       table = this.deleteBlankRowFromTable(table);
       if (table.length === 0) return "なし";
 
-      // 末尾の余計な空白を削除
+      // 末尾の空白を削除
       table = this.deleteTrailingBlankFromTable(table);
 
+      // カラム列に合わせて末尾に空白をセット
+      const columnLength = this.firstLineName
+        ? table[1].length
+        : table[0].length;
+      table = table.map(row => this.resizeArray(row, columnLength, ""));
+
       if (this.firstLineName) {
+        // 1行目がテーブル名なら、取得してセットしてデータテーブルから外す
         this.tableName = table[0].filter(col => col !== "")[0];
         table = table.slice(1);
       }
 
+      // テーブル名を用意
+      const tableName =
+        this.dbType === "MySQL"
+          ? this.tableName
+          : this.dbType === "SQL Server"
+          ? "[" + this.tableName + "]"
+          : this.dbType === "Oracle"
+          ? '"' + this.tableName + '"'
+          : this.tableName;
+
       // カラム列を用意
-      let columnLine = "";
-      if (this.DbType === "SQL Server") {
-        columnLine = this.encloseColumnName_SqlServer(table[0]).join(",");
-      } else if (this.DbType === "MySQL") {
-        columnLine = this.encloseColumnName_Mysql(table[0]).join(",");
-      }
+      const columns =
+        this.dbType === "MySQL"
+          ? this.encloseColumnName_Mysql(table[0])
+          : this.dbType === "SQL Server"
+          ? this.encloseColumnName_SqlServer(table[0])
+          : this.dbType === "Oracle"
+          ? this.encloseColumnName_Oracle(table[0])
+          : [];
+
       table = table.slice(1);
 
       // データ列を用意
       const dataTable = this.encloseData(table);
 
       // クエリを組み立てる
-      const query =
-        "INSERT INTO " +
-        this.tableName +
-        "\n    (" +
-        columnLine +
-        ")" +
-        "\nVALUES" +
-        "\n    " +
-        dataTable.map(cols => "(" + cols.join(",") + ")").join("\n    ,") +
-        ";";
+      let query = "";
+      if (["SQL Server", "MySQL"].some(dBType => dBType === this.dbType)) {
+        query =
+          "INSERT INTO " +
+          tableName +
+          " (" +
+          columns.join(",") +
+          ")" +
+          "\nVALUES " +
+          dataTable.map(cols => "(" + cols.join(",") + ")").join("\n    ,") +
+          ";";
+      }
+      if (this.dbType === "Oracle") {
+        if (dataTable.length <= 1) {
+          query += "INSERT ";
+        } else {
+          query += "INSERT ALL\n";
+        }
+
+        query += dataTable
+          .map(cols => {
+            let intoQuery =
+              "INTO " +
+              tableName +
+              " (" +
+              columns.join(",") +
+              ")" +
+              " VALUES (" +
+              cols.join(",") +
+              ")";
+            return intoQuery;
+          })
+          .join("\n");
+
+        if (dataTable.length > 1) {
+          query += "\nSELECT * FROM DUAL;";
+        }
+      }
 
       return query;
     }
@@ -114,6 +160,10 @@ export default Vue.extend({
     /** カラム名を文字として扱うよう囲う(SQL Server) */
     encloseColumnName_SqlServer(cols: string[]): string[] {
       return cols.map(col => "[" + col + "]");
+    },
+    /** カラム名を文字として扱うよう囲う(Oracle) */
+    encloseColumnName_Oracle(cols: string[]): string[] {
+      return cols.map(col => '"' + col + '"');
     },
 
     /** 2次元配列から空行を削除 */
@@ -136,7 +186,12 @@ export default Vue.extend({
     /** 2次元配列から末尾が空文字の項を削除 */
     deleteTrailingBlankFromTable(table: string[][]): string[][] {
       return table.map(row => {
-        const index = row.length - row.slice().reverse().findIndex(col => col !== "");
+        const index =
+          row.length -
+          row
+            .slice()
+            .reverse()
+            .findIndex(col => col !== "");
         if (row.length < index) return [];
         return row.slice(0, index);
       });
@@ -208,6 +263,20 @@ export default Vue.extend({
     /** TSV→2次元配列 */
     combartTsvToTable(tsv: string): string[][] {
       return tsv.split("\n").map(row => row.split("\t"));
+    },
+
+    /** 配列の長さを変える
+     *
+     * - 元の配列より長くする：末尾に初期値を埋めて長くする
+     * - 短くする：末尾を切り落とす
+     * - 同じサイズ：何もしない
+     * lengthにマイナスを入れるとUB。
+     */
+    resizeArray(arr: any[], length: number, initValue: any): any[] {
+      const len = length - arr.length;
+      if (len === 0) return arr;
+      if (len < 0) return arr.slice(0, length);
+      return arr.concat(Array(len).fill(initValue));
     },
 
     /** タブ入力をさせる */
